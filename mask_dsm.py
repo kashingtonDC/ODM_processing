e483245d15e16d2f5b53912468b3d831d9f101c4
Aakash Ahamed <ahamednasa@gmail.com>
2022-04-02 11:58:48 -0700

    add RGBA channels to DEM for google earth visualizing

--- mask_dsm.py
+++ mask_dsm.py
@@ -3,6 +3,7 @@ import sys
 import imageio
 import numpy as np
 import rasterio as rio
+import matplotlib.pyplot as plt
 
 if len(sys.argv) == 1:
 	print("Please supply image directory as argument, exiting")
@@ -17,11 +18,33 @@ for fn in [dtm_fn, dsm_fn]:
 	src = rio.open(fn)
 	profile = src.profile
 	arr = src.read(1)
+	# Get mask set alpha channel
+	alpha = arr!=src.nodata
+	alpha = alpha.astype(np.uint8)
+	alpha[alpha==1] = 255
+	# mask nodata 
 	arr[arr==src.nodata] = np.nan
+	# scale vals bt 0-255
+	arr = ((arr - np.nanmin(arr)) * (1/(np.nanmax(arr) - np.nanmin(arr)) * 255)).astype('uint8')
 
+	arr[arr==np.nan] = 0
+
+	# 4 channel stack (RGBA)
+	rgb = np.dstack([arr,arr,arr, alpha])
+	rgb = np.moveaxis(rgb, [0, 1, 2], [1, 2, 0])
 	outfn = os.path.join(dem_dir,os.path.split(fn)[-1].replace(".","_masked."))
-
-	with rio.open(outfn, 'w', **profile) as dst:
-		dst.write(arr, 1)
-		
+	
+	# write out
+	with rio.open(outfn, 'w',
+		height=rgb.shape[1],
+		width=rgb.shape[2],
+		count=4,
+		# dtype=profile['dtype'],
+		dtype = np.uint8,
+		nodata=0,
+		transform = profile['transform'],
+		crs = profile['crs'],
+		compress='deflate') as dst:
+			dst.write(rgb)
+			
 	print("Wrote {} =========================== ".format(outfn))
