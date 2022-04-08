3c00558184a5fb2e9a72245405cc2ce351894ff7
Aakash Ahamed <ahamednasa@gmail.com>
2022-04-07 21:40:14 -0700

    exception handling for file not existing

--- mask_dsm.py
+++ mask_dsm.py
@@ -15,36 +15,45 @@ dtm_fn = os.path.abspath(os.path.join(dem_dir,"dtm.tif"))
 dsm_fn = os.path.abspath(os.path.join(dem_dir,"dsm.tif"))
 
 for fn in [dtm_fn, dsm_fn]:
-	src = rio.open(fn)
-	profile = src.profile
-	arr = src.read(1)
-	# Get mask set alpha channel
-	alpha = arr!=src.nodata
-	alpha = alpha.astype(np.uint8)
-	alpha[alpha==1] = 255
-	# mask nodata 
-	arr[arr==src.nodata] = np.nan
-	# scale vals bt 0-255
-	arr = ((arr - np.nanmin(arr)) * (1/(np.nanmax(arr) - np.nanmin(arr)) * 255)).astype('uint8')
+	try:
+		src = rio.open(fn)
+		profile = src.profile
+		arr = src.read(1)
+		# Get mask set alpha channel
+		alpha = arr!=src.nodata
+		alpha = alpha.astype(np.uint8)
+		alpha[alpha==1] = 255
+		# mask nodata 
+		arr[arr==src.nodata] = np.nan
+		# scale vals bt 0-255
+		arr = ((arr - np.nanmin(arr)) * (1/(np.nanmax(arr) - np.nanmin(arr)) * 255)).astype('uint8')
 
-	arr[arr==np.nan] = 0
+		arr[arr==np.nan] = 0
 
-	# 4 channel stack (RGBA)
-	rgb = np.dstack([arr,arr,arr, alpha])
-	rgb = np.moveaxis(rgb, [0, 1, 2], [1, 2, 0])
-	outfn = os.path.join(dem_dir,os.path.split(fn)[-1].replace(".","_masked."))
-	
-	# write out
-	with rio.open(outfn, 'w',
-		height=rgb.shape[1],
-		width=rgb.shape[2],
-		count=4,
-		# dtype=profile['dtype'],
-		dtype = np.uint8,
-		nodata=0,
-		transform = profile['transform'],
-		crs = profile['crs'],
-		compress='deflate') as dst:
-			dst.write(rgb)
-			
-	print("Wrote {} =========================== ".format(outfn))
+		# 4 channel stack (RGBA)
+		rgb = np.dstack([arr,arr,arr, alpha])
+		rgb = np.moveaxis(rgb, [0, 1, 2], [1, 2, 0])
+		outfn = os.path.join(dem_dir,os.path.split(fn)[-1].replace(".","_masked."))
+		
+		# write out
+		with rio.open(outfn, 'w',
+			height=rgb.shape[1],
+			width=rgb.shape[2],
+			count=4,
+			# dtype=profile['dtype'],
+			dtype = np.uint8,
+			nodata=0,
+			transform = profile['transform'],
+			crs = profile['crs'],
+			compress='deflate') as dst:
+				dst.write(rgb)
+
+				# dst.write_colormap(
+				# 1, {
+				# 	0: (255, 0, 0, 255),     
+				# 	255: (0, 0, 255, 255) })
+				
+		print("Wrote {} =========================== ".format(outfn))
+	except:
+		print("{} DOES NOT EXIST, SKIPPING TO NEXT ELEV / TERRAIN MODEL".format(fn))
+		continue
